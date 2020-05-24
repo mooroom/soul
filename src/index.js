28dbe72bf2835e458f58ed0de85ea2467f9bc5ab
mooroom <61869246+mooroom@users.noreply.github.com>
2020-05-25 03:28:34 +0900

    0524
    0524

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { FirebaseAppProvider } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAZRl77_R00AlH6K1vjqQ52Mq5M8ukH1E",
+  authDomain: "soul-67206.firebaseapp.com",
+  databaseURL: "https://soul-67206.firebaseio.com",
+  projectId: "soul-67206",
+  storageBucket: "soul-67206.appspot.com",
+  messagingSenderId: "494733846247",
+  appId: "1:494733846247:web:6ba8a3dcdc0d8bbb87e442",
+  measurementId: "G-HSR0HQXV1B",
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig} initPerformance>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </FirebaseAppProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
