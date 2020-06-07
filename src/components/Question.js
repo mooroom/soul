59c3f22a55bd5afe158c91e1ae7cee274052ec66
mooroom <61869246+mooroom@users.noreply.github.com>
2020-06-08 03:21:06 +0900

    0607 imac
    step 호출 순서가 문제

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,26 +3,36 @@ import Option from "./Option";
 
 const Question = ({ question, step, handleCheckedVal, render, nextStep }) => {
   const [options, setOptions] = useState([
-    { id: 1, value: "1", isChecked: false },
-    { id: 2, value: "2", isChecked: false },
-    { id: 3, value: "3", isChecked: false },
-    { id: 4, value: "4", isChecked: false },
-    { id: 5, value: "5", isChecked: false },
+    { id: 1, value: 1, isChecked: false },
+    { id: 2, value: 2, isChecked: false },
+    { id: 3, value: 3, isChecked: false },
+    { id: 4, value: 4, isChecked: false },
+    { id: 5, value: 5, isChecked: false },
   ]);
 
-  const [selected, setSelected] = useState({
-    selectedVal: "",
-  });
+  const [selected, setSelected] = useState(null);
 
   const handleChecked = (id) => {
     let newOptions = options;
     newOptions.forEach((option) => {
       if (option.id === id) {
         option.isChecked = !option.isChecked;
-        setSelected({ selectedVal: option.id });
       }
     });
     setOptions(newOptions);
+    handleSelected();
+
+    console.log(options);
+    console.log(selected);
+  };
+
+  const handleSelected = () => {
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].isChecked === false) {
+        setSelected(options[i].value);
+      }
+    }
+    console.log(selected);
   };
 
   return (
@@ -46,7 +56,10 @@ const Question = ({ question, step, handleCheckedVal, render, nextStep }) => {
           type="button"
           className="btn purple d-block m-auto"
           style={{ color: "white" }}
-          onClick={nextStep}
+          onClick={() => {
+            nextStep();
+            handleCheckedVal(step, selected);
+          }}
         >
           다음으로
         </button>
--- src/components/Test.js
+++ src/components/Test.js
@@ -19,82 +19,89 @@ const Test = () => {
   const currentStep = useRef(1);
   const checkedValueSum = useRef([]);
 
+  const [types, setTypes] = useState({
+    typeA: 0,
+    typeB: 0,
+    typeC: 0,
+    typeD: 0,
+  });
+
   const [questions, setQuestions] = useState([
     {
       id: 1,
       title: "나는 우울할 때 일부러 사람을 만나는 편이다.",
-      checkedVal: "",
+      checkedVal: null,
       render: true,
     },
     {
       id: 2,
       title: "나는 나의 상태에 누군가 관심을 가져줬으면 좋겠다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 3,
       title: "혼자 있으면 우울에 침잠 되는 기분이 든다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 4,
       title: "몸을 움직이는 게 기분 전환에 도움이 된다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 5,
       title: "운동을 꾸준히 해 본 경험이 있다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 6,
       title: "하루 평균 6000보 이상 걷는다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 7,
       title: "내 취향의 음악을 찾고 듣는 것을 좋아한다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 8,
       title: "미술 전시, 작품 보는 것을 좋아한다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 9,
       title: "영상미 넘치는 영상을 유튜브에서 즐겨 본다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 10,
       title: "일기, 글 쓰기를 통해 내 생각을 잘 표현하는 편이다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 11,
       title: "주변 사람들은 내 일상과 생각에 대해 잘 알고 있다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
     {
       id: 12,
       title: "나는 우울할 때 일부러 사람을 만나는 편이다.",
-      checkedVal: "",
+      checkedVal: null,
       render: false,
     },
   ]);
 
-  const handleCheckedVal = (step) => (selectedVal) => {
+  const handleCheckedVal = (step, selectedVal) => {
     setQuestions(
       questions.map((question) =>
         question.id === step
@@ -102,6 +109,7 @@ const Test = () => {
           : question
       )
     );
+    console.log(questions);
   };
 
   const nextStep = () => {
@@ -114,9 +122,8 @@ const Test = () => {
         questions[i].render = false;
       }
     }
-    console.log(currentStep.current);
+    //console.log(currentStep.current);
     checkedValueSum.current.push(questions[currentStep.current - 1].id);
-    console.log(checkedValueSum);
   };
 
   return (
