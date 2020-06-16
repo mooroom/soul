14b39465eda41128fa0b728dd049ef4707ddd7be
mooroom <61869246+mooroom@users.noreply.github.com>
2020-06-17 04:02:29 +0900

    0616 imac

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { Redirect } from "react-router";
 import { useAuth, useFirestoreDocData, useFirestore } from "reactfire";
 import Menu from "./components/Menu";
+import "./App.scss";
+import logo from "./img/logo.svg";
+import Slider from "infinite-react-carousel";
 
 const Home = () => {
   var firestore = useFirestore();
@@ -13,12 +16,35 @@ const Home = () => {
 
   const name = useFirestoreDocData(userDoc).uName;
 
+  const settings = {
+    arrows: false,
+    autoplay: true,
+    dots: false,
+  };
   return (
-    <div>
-      <Menu />
-      <h2>{name}님, 오늘 하루도 잘 버티고 계신가요?</h2>
-      <button onClick={() => auth.signOut()}>로그아웃</button>
-    </div>
+    <>
+      <Menu name={name} />
+      <div className="container">
+        <img className="mb-3" src={logo} width="70px" />
+        <h5>{name}님,</h5>
+        <h5>오늘도 잘 버티고 계신가요?</h5>
+
+        <div className="todayBox">
+          <h6>{name}을 위한 오늘의 추천 컨텐츠</h6>
+
+          <div className="soulCard">
+            <div className="imgSection">
+              <img src={logo} width="50px" />
+            </div>
+            <div className="txtSection">
+              <p>#에세이#새벽#사랑</p>
+              <p>사람을 대하는 가장 적정한 온도, [나의 친애하는 적] 허지웅</p>
+              <p>2020.06.20</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,41 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import "./Menu.css";
+import "./Menu.scss";
+import profile from "../img/profile.svg";
+import { useAuth } from "reactfire";
 
-const Menu = () => {
+const Menu = ({ name }) => {
   const [active, setActive] = useState(false);
   const onToggle = () => {
     setActive(!active);
   };
+  var auth = useAuth();
 
   return (
     <div>
-      <button
-        className="btn btn-primary"
-        id="menuBtn"
-        onClick={onToggle}
-      ></button>
+      <div id="menuBtn" onClick={onToggle}>
+        <div id="top"></div>
+        <div id="middle"></div>
+        <div id="bottom"></div>
+      </div>
+      <div id="menuBox" className={`${active ? "active" : ""}`}>
+        <img className="mb-3" src={profile} width="70px" />
+        <h6>
+          <b>{name}</b>
+        </h6>
+        <h6 className="mb-5">소울 새내기</h6>
+        <div id="infoBox">
+          <h6 className="mb-2">이벤트 참여 6회</h6>
+          <h6>감정기록 15회</h6>
+        </div>
+        <div id="infoBox2">
+          <h6>연계기관 보기</h6>
+          <h6 className="mb-5">고객센터</h6>
+          <button className="btn btn-light" onClick={() => auth.signOut()}>
+            로그아웃
+          </button>
+        </div>
+      </div>
       <div
         id="pageWrapper"
         className={`${active ? "active" : ""}`}
