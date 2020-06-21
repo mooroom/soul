3d16e9a7a35867c51b5b6b992aa6b3796c21dfaf
mooroom <61869246+mooroom@users.noreply.github.com>
2020-06-22 03:47:54 +0900

    imac 0621-2

--- src/Write.js
+++ src/Write.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "./components/Head";
 import { useAuth, useFirestoreDocData, useFirestore } from "reactfire";
 import ProfileCard from "./ProfileCard";
@@ -16,6 +16,20 @@ const Write = () => {
 
   const name = useFirestoreDocData(userDoc).uName;
   const type = useFirestoreDocData(userDoc).myType;
+
+  const [active, setActive] = useState(false);
+  const handleActive = () => {
+    setActive(!active);
+  };
+
+  const handleArchive = () => {};
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div>
       <Head title="데일리 아카이브" />
@@ -23,7 +37,7 @@ const Write = () => {
         <ProfileCard profile={profile} name={name} type={type} />
         <div id="addEmotionBox" className="mt-4">
           <div className="f-18 mb-4">오늘의 기분을 기록하세요</div>
-          <img src={addBtn} width="35px" />
+          <img src={addBtn} width="35px" onClick={() => handleActive()} />
         </div>
         <Carousel indicators={false} className="mt-5">
           <Carousel.Item>
@@ -37,9 +51,23 @@ const Write = () => {
           </Carousel.Item>
         </Carousel>
       </div>
-      {/* <div id="writeBox">
+      <div id="writeBox" className={active ? "active" : ""}>
         <div className="f-18 mb-4">오늘의 나는?</div>
-      </div> */}
+        <div id="diaryCard">
+          <div className="f-14 black text-center mb-3">{dateString}</div>
+          <button onClick={handleActive}>x</button>
+          <form onSubmit={handleArchive} className="text-center">
+            <textarea
+              className="form-control mb-2"
+              placeholder="텍스트를 입력하세요"
+              rows="3"
+            ></textarea>
+            <button className="soul-btn-blue" type="submit">
+              등록
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
